bb3b9eabdab492bda58e04e43da31ac8b70c5fce
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-02-12 21:23:22 +0100

    Modified Button color

--- src/components/form/SupportBtn.js
+++ src/components/form/SupportBtn.js
@@ -3,7 +3,7 @@ import { LoveIcon } from './../../assets/svgsIcons';
 
 const SupportBtn = ({ title }) => {
     return (
-        <button type="button" className="text-white bg-tdf-blue-50 hover:bg-tdf-blue-100 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center">
+        <button type="button" className="text-white bg-tdf-blue-50 hover:bg-gray-800 font-medium rounded-lg text-lg px-5 py-2.5 text-center inline-flex items-center">
             {title}
             <span className="ml-2 -mr-1">
                 <LoveIcon className=" w-4 h-4" />
